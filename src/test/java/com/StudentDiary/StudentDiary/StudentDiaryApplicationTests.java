df63162860bccd452d18b2267a847984b84c0fd7
VitaliiPriadilia <vitalik11087625@gmail.com>
2023-08-17 12:01:06 +0200

    Added clean project

---
+++ src/test/java/com/StudentDiary/StudentDiary/StudentDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentDiary.StudentDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
